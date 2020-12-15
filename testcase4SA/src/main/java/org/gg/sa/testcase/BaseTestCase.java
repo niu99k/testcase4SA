3f3a421ee5b7d10afd8e1ed77fb81a359bfe5838
GG <gg_2p_2a@163.com>
2020-12-15 20:37:44 +0800

    BaseTestCase

---
+++ testcase4SA/src/main/java/org/gg/sa/testcase/BaseTestCase.java
@@ -0,0 +1,11 @@
+package org.gg.sa.testcase;
+
+import com.gg.entity.*;
+
+import java.util.*;
+
+public abstract class BaseTestCase {
+    private List<Machine> machineList;
+    private List<Job> jobList;
+
+}
---
+++ testcase4SA/src/main/java/org/gg/sa/testcase/TestCase1.java
@@ -0,0 +1,9 @@
+package org.gg.sa.testcase;
+
+/*
+1/prec/fmax
+ */
+public class TestCase1 {
+    public TestCase1(){
+}
+}
